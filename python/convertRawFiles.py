50333035b3ed8eee001aaa34c17e711b37ef232b
Adrien Blanchet <adrien.blanchet@live.fr>
2025-09-04 11:43:14 +0200

    checking if file exists

--- python/convertRawFiles.py
+++ python/convertRawFiles.py
@@ -6,11 +6,13 @@ import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description='Convert .dat files in specified folder')
-    parser.add_argument('-i', '--input', type=str, required=True,
+    parser.add_argument('-i', type=str, required=True,
                         help='Input folder path containing .dat files')
+    parser.add_argument("-o", type=str, required=True, help="Output folder path")
 
     args = parser.parse_args()
     input_folder = Path(args.input)
+    output_folder = Path(args.o)
 
     if not input_folder.exists():
         print(f"Error: Input folder '{input_folder}' does not exist.")
@@ -29,7 +31,16 @@ def main():
     print(f"Processing CAL files ({len(cal_files)}):")
     for file_path in cal_files:
         print(f"- {file_path.name}")
-        subprocess.run(['bmRawToRootConverter', '-i', str(file_path), '--is-calib', '--skip-event'])
+
+        if not os.path.exists(os.path.join(output_folder, file_path.name.replace('.dat', '.root'))):
+            continue
+
+        subprocess.run([
+            'bmRawToRootConverter',
+            '-i', str(file_path),
+            '--is-calib', '--skip-event',
+            '-o', output_folder
+        ])
 
     # print(f"Processing BEAM files ({len(beam_files)}):")
     # for file_path in beam_files:
